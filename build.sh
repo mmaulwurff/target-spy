e2555f1882a2cfe4b7535300269d4f4ceea08f51
Alexander Kromm <mmaulwurff@gmail.com>
2019-12-07 19:57:43 +0400

    clean up build script

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+set -e
+
 name=target-spy
 
-rm -f $name.pk3 \
-&& \
+#iwad=
+map=test
+
+rm -f $name.pk3
+
 git log --date=short --pretty=format:"-%d %ad %s%n" | \
     grep -v "^$" | \
     sed "s/HEAD -> master, //" | \
@@ -11,8 +16,8 @@ git log --date=short --pretty=format:"-%d %ad %s%n" | \
     sed "s/ (HEAD -> master)//" | \
     sed "s/ (origin\/master)//"  |\
     sed "s/- (tag: \(v\?[0-9.]*\))/\n\1\n-/" \
-    > changelog.txt \
-&& \
+    > changelog.txt
+
 zip $name.pk3        \
     graphics/*.lmp   \
     graphics/*.png   \
@@ -24,18 +29,12 @@ zip $name.pk3        \
     *.txt  \
     *.md   \
     *.fon2 \
-    *.lmp  \
-&& \
-cp $name.pk3 $name-$(git describe --abbrev=0 --tags).pk3 \
-&& \
-gzdoom -glversion 3 \
-       \ #-iwad ~/Programs/Games/wads/doom/freedoom1.wad \
-       \ #-iwad ~/Programs/Games/wads/doom/freedoom2.wad \
-       -file \
-       $name.pk3 \
-       ~/Programs/Games/wads/maps/test/DOOMTEST.wad \
-       "$1" "$2" \
-       +map test \
-       \ #-nomonsters \
+    *.lmp
+
+cp $name.pk3 $name-$(git describe --abbrev=0 --tags).pk3
+
+gzdoom -iwad $iwad \
+       -file $name.pk3 "$1" "$2" "$3" \
+       +map $map \
        +notarget \
        +summon doomimp
